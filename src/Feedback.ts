b1c8f016ca92c5bba5745a92eb4d586ace023ef5
Oeub ROCHOM <oeub.rochom@student.passerellesnumeriques.org>
2025-05-30 15:16:58 +0700

    created class Feedback and assigned properties

---
+++ src/Feedback.ts
@@ -0,0 +1,68 @@
+import { Rating } from "./Rating";
+import { Student } from "./Student";
+import { Subject } from "./Subject";
+import { Teacher } from "./Teacher";
+
+export class Feedback{
+    constructor(
+
+        private id: number, 
+        private student: Student, 
+        private subject: Subject, 
+        private teacher: Teacher, 
+        private rating: Rating, 
+        private comment: string,
+    )
+    {}
+
+    getId(): number {
+        return this.id;
+    }
+
+    setId(id: number): void {
+        this.id = id;
+    }
+
+
+    getStudent(): Student {
+        return this.student;
+    }
+
+    setStudent(student: Student): void {
+        this.student = student;
+    }
+
+    getSubject(): Subject {
+        return this.subject;
+    }
+    
+    setSubject(subject: Subject): void {
+        this.subject = subject;
+    }
+
+    getTeacher(): Teacher {
+        return this.teacher;
+    }
+
+    setTeacher(teacher: Teacher): void {
+        this.teacher = teacher;
+    }
+
+    getRating(): Rating {
+        return this.rating;
+    }
+
+    setRating(rating: Rating): void {
+        this.rating = rating;
+    }
+
+    getComment(): string {
+        return this.comment;
+    }
+
+    setComment(comment: string): void {
+        this.comment = comment;
+    }
+
+    save(): void {}
+}
